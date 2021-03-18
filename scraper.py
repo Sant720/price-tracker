375b6236261d6b90d794efab3287ad62b97dde02
sant720 <santiago.bolivar7@gmail.com>
2021-03-17 19:04:13 -0500

    Move scraping code into functions

--- scraper.py
+++ scraper.py
@@ -1,21 +1,31 @@
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.amazon.com/AKG-Pro-Audio-K92-Closed-Back/dp/B01AIO8XVA/?_encoding=UTF8&pd_rd_w=whu2F&pf_rd_p=49ff6d7e-521c-4ccb-9f0a-35346bfc72eb&pf_rd_r=GE1E8E3KRHC43N354ZY6&pd_rd_r=509d7a96-e005-4c1c-86a0-2862d3a7d031&pd_rd_wg=NLSwX&ref_=pd_gw_ci_mcx_mr_hp_d"
+def main():
+    title, price = get_data()
+    print(title)
+    print(price)
 
-headers = {
-    "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36', 
-    "Accept-Encoding":"gzip, deflate", 
-    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 
-    "DNT":"1",
-    "Connection":"close", 
-    "Upgrade-Insecure-Requests":"1"}
+def get_data():
+    # Will collect URl from user input in the future. This is a test URl
+    URL = "https://www.amazon.com/AKG-Pro-Audio-K92-Closed-Back/dp/B01AIO8XVA/?_encoding=UTF8&pd_rd_w=whu2F&pf_rd_p=49ff6d7e-521c-4ccb-9f0a-35346bfc72eb&pf_rd_r=GE1E8E3KRHC43N354ZY6&pd_rd_r=509d7a96-e005-4c1c-86a0-2862d3a7d031&pd_rd_wg=NLSwX&ref_=pd_gw_ci_mcx_mr_hp_d"
 
-page = requests.get(URL, headers=headers)
-soup = BeautifulSoup(page.content, 'html.parser')
+    # Headers are used to pass as real user and prevent blocks from website
+    headers = {
+        "User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Safari/537.36', 
+        "Accept-Encoding":"gzip, deflate", 
+        "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", 
+        "DNT":"1",
+        "Connection":"close", 
+        "Upgrade-Insecure-Requests":"1"}
 
-title = soup.find(id="productTitle").get_text()
-price = soup.find(id="priceblock_ourprice").get_text()
+    page = requests.get(URL, headers=headers)
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-print(title.strip())
-print(price.strip())
+    title = soup.find(id="productTitle").get_text()
+    price = soup.find(id="priceblock_ourprice").get_text()
+    title, price = title.strip(), float(price.strip()[1:])
+    return title, price
+
+if __name__ == "__main__":
+    main()
